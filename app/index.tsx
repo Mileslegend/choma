b611d8254640a0a5c700f9cff489ebb6f119ba07
Mileslegend <mileslegend7@gmail.com>
2025-07-09 08:23:40 +0000

    initialised custom styles

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+      <Text className="text-5xl font-bold text-center font-quicksand-bold text-primary text-rose-500">
         Welcome dear Miles King 😊!
       </Text>
     </View>
