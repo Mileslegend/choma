dc4bb6dd82243bb96b9e90da5ddcfe3f134114df
champ <mileslegend7@gmail.com>
2025-07-10 20:25:35 +0300

    auth UI set up

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     platform: 'com.muhuan.choma',
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: '686f9b2400261bbadddc',
+    userCollectionId: '686f9b70002ca1ba3b94'
 }
